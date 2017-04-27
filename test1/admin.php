50e195485b08d30074bdadd671896ad02f2ba3f9
yur14 <yurii.simchuk@yahoo.com>
2017-04-27 19:27:10 +0300

    Create admin.php

---
+++ test1/admin.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Форма</title>
+</head>
+<body>
+<form action="upload.php" method="post" id="uploadJson" enctype="multipart/form-data">
+    Выберите файл для загрузки:
+    <input type="file" name="testJson" id="testJson">
+    <button name="submit" class="btn btn-primary" type="submit" value="submit">Загрузить</button>
+</form>
+</form>
+</body>
+</html>
