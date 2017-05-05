854dff519c738ac4ef548c551b64ec77ad455014
yur14 <yurii.simchuk@yahoo.com>
2017-05-05 20:20:49 +0300

    Update array3.php

--- array3.php
+++ array3.php
@@ -1,102 +1,99 @@
-<!DOCTYPE html>
-<html>
 <?php
-error_reporting(E_ALL);
-?>
-<head>
-  <meta charset="utf-8">
-  <title>Животные</title>
-</head>
-<body>
+$animals = [
+    "Africa"     => ["Philantomba monticola", "Lemuridae", "Panthera pardus", "Rhinocerotidae", "Giraffa camelopardalis"],
+    "Australia"  => ["Sarcophilus laniarius", "Canis dingo", "Vombatidae", "Dacelo gaudichaud", "Ornithorhynchus anatinus"],
+    "Antarctica" => ["Phocidae", "Balaenoptera musculus", "Oceanodroma leucorhoa", "Stercorarius skua", "Spheniscidae"]
+           ];
 
-<h1>Животные</h1>
-
-<?php
-$Eurasia_animals = array("Giant Panda", "Gharial crocodile", "Mouflon");
-$Africa_animals = array("African Elephant", "Arabian Camel", "Assassin Bug");
-$North_A_animals = array("Agouti", "Alaskan Malamute");
-$South_A_animals = array("Black Caiman", "Capybara", "Common Iguana", "Dogfish Shark");
-$Australia_animals = array("Black Swan", "Dingo", "Frilled Lizard", "Kangaroo", "Minke Whale");
-$Antarctica_animals = array("Antarctic Krill", "Arctic Tern");
-
-$All_animals = array ("Eurasia" => $Eurasia_animals,
-                      "Africa" => $Africa_animals,
-                      "NorthAmerica" => $North_A_animals,
-                      "SouthAmerica" => $South_A_animals,
-                      "Australia" => $Australia_animals,
-                      "Antarctica" => $Antarctica_animals
-                     );
-
-
-//перемешать слова в массиве
-function mixer ($arr_mx,$key)
-{
-//преобразуем в строку
-    $im_str = implode(' ', $arr_mx[$key]);
-//преобразуем обратно в массив - разделитель пробел, т.е. разделяем массив по одному слову
-    $ex_str = explode(' ', $im_str);
-//перемешиваем массив
-    $x=shuffle($ex_str);
-//возвращаем значение
-    return $ex_str;
+$newAnimals     = [];
+$firstWord      = [];
+$secondWord     = [];
+$fantasyAnimals = [];
+/////////////////////////////////////////////////
+foreach ($animals as $continent => $value) {
+    if($continent == "Africa") {
+        foreach($value as $africanAnimals) {
+            $start = strpos($africanAnimals, ' ');
+            $cut = substr($africanAnimals, $start);
+            $africanAnimals = str_replace($cut, '', $africanAnimals);
+            if($africanAnimals == ''){
+                continue;
+            }
+            $firstWordAfrica[] = $africanAnimals;
+        }
+    }
+    if($continent == "Australia") {
+        foreach($value as $australianAnimals) {
+            $start = strpos($australianAnimals, ' ');
+            $cut = substr($australianAnimals, $start);
+            $australianAnimals = str_replace($cut, '', $australianAnimals);
+            if($australianAnimals == ''){
+                continue;
+            }
+            $firstWordAustralia[] = $australianAnimals;
+        }
+    }
+    if($continent == "Antarctica") {
+        foreach($value as $antarcticanAnimals) {
+            $start = strpos($antarcticanAnimals, ' ');
+            $cut = substr($antarcticanAnimals, $start);
+            $antarcticanAnimals = str_replace($cut, '', $antarcticanAnimals);
+            if($antarcticanAnimals == ''){
+                continue;
+            }
+            $firstWordAntarctica[] = $antarcticanAnimals;
+        }
+    }
+    foreach ($value as $animalsName) {
+        if(strpos($animalsName, ' ')) {
+            $newAnimals[] = $animalsName;
+        }
+    }
 }
-
-//достать случайное слово из массива
-function one_word ($arr_ow)
-{
-//получаем случайный ключ массива
-$k=array_rand($arr_ow, 1);
-//достаем слово по ключу
-$word = $arr_ow[$k];
-//возвращаем значение
-return $word;
+//////////////////////////////////////////////////
+foreach ($newAnimals as $string) {
+    $start = strpos($string, ' ');
+    $cut = substr($string, $start);
+    $string = str_replace($cut, '', $string);
+    $firstWord[] = $string;
+    $secondWord[] = $cut;
 }
+$shuffle = shuffle($secondWord);
+$animals["Africa"]     = [];
+$animals["Australia"]  = [];
+$animals["Antarctica"] = [];
 
-//создание общего нового массива в котором только названия животных состоящих из 2-х слов
-foreach($All_animals as $key => $vol)
-{
-   for($i=0; $i < count($All_animals[$key]); $i++)
-   {
-    //++поиск пробела - все животные состоящие из 2-х слов
-    $space = (strrpos($All_animals[$key][$i], ' '));
-        if ($space === false) {}
-        else
-             {
-    //++запись в новый массив
-    $tmp[$key][$i]=$All_animals[$key][$i];
-             }
+for ($i = 0; $i<count($firstWord); $i++) {
+    $fantasyAnimals[] = "$firstWord[$i] $secondWord[$i]";
+    if (isset($firstWordAfrica[$i])) {
+        if ($firstWord[$i] === $firstWordAfrica[$i] or $firstWord[$i] === $firstWordAfrica[$i] or $firstWord[$i] === $firstWordAfrica[$i]) {
+            $animals["Africa"][$i] = $firstWord[$i] . $secondWord[$i];
+        }
+    }
+    if (isset($firstWordAustralia[$i])) {
+        if ($firstWord[$i+count($animals)] === $firstWordAustralia[$i] or $firstWord[$i] === $firstWordAustralia[$i] or $firstWord[$i] === $firstWordAustralia[$i] or $firstWord[$i] === $firstWordAustralia[$i]) {
+            $animals["Australia"][$i] = $firstWord[$i+count($animals)] . $secondWord[$i];
+        }
     }
 }
 
-//работаем с новым массивом
-foreach($tmp as $key => $vol)
-{
-
-//выводим континент
-echo "<h2>".$key."</h2>";
-
-//в $res уже перемешанные значения - осталось их сложить по словам с другим массивом
-$res[$key]=mixer($tmp,$key);
-//достаем случайный ключ из массива $tmp
-$rnd_key=array_rand($tmp);
-
-for($i=0; $i < count($res[$key]); $i++)
-    {
-//получаем случайное слово из случайного массива
-    $w1=one_word(mixer($tmp,$rnd_key));
-//сложение элементов массива
-    $res1[$key][$i]=$res[$key][$i]." ".$w1;
-//выводим на экран
-    echo $res1[$key][$i];
-//не рисовать запятую в конце строки
-     if ($i<count($res[$key])-1)
-        {
-         echo ", ";
-        }
-     }
+foreach($animals as $value) {
+    foreach($value as $animalName) {
+        $animalNamed[] = $animalName;
+    }
 }
 
-?>
-
-</body>
-</html>
+for ($i = 0; $i<count($firstWord); $i++) {
+    if (isset($firstWordAntarctica[$i])) {
+        if ($firstWord[$i + count($animalNamed)] === $firstWordAntarctica[$i] or $firstWord[$i] === $firstWordAntarctica[$i] or $firstWord[$i] === $firstWordAntarctica[$i]) {
+            $animals["Antarctica"][$i] = $firstWord[$i + count($animalNamed)] . $secondWord[$i];
+        }
+    }
+}
+//////////////////////////////////////////////////
+echo "<h1>Животные:</h1>";
+foreach ($animals as $key => $newContinent) {
+    echo "<h2>$key</h2>";
+    $result = implode(", ", $newContinent);
+    echo $result;
+}
