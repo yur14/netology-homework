3bd2a23a7ed3d5e191ac2524fc531c56b346d00e
yur14 <yurii.simchuk@yahoo.com>
2017-05-14 09:55:39 +0300

    Update array3.php

--- array3.php
+++ array3.php
@@ -1,41 +1,25 @@
 <?php
-error_reporting(E_ALL);
-$allAnimals = array(
-        "Eurasia"       => array("Red deer", "Pond turtle", "Indian Rhinoceros", "Praying Mantis", "Bison", "Anser"),
-        "Africa"        => array("Fennec Fox", "Forest Giraffe", "Golden Eagle", "Hedgehog", "Redbilled Oxpecker"),
-        "North America" => array("Bighorn Sheep", "Blue Heron", "Bobcat", "Brown Pelican", "Canada Goose", "Crocodile"),
-        "South America" => array("Hare", "Hermit Crab", "Lowland Tapir", "Paint Horse", "Plain Xenops"),
-        "Australia"     => array("Red Kangaroo", "Sugar Glider", "Ringtail Possum", "Tasmanian Tiger", "Wombat"),
-        "Antarctica"    => array("Octopus", "Penguin", "Sea Star", "Spectacled Porpoise", "Squid", "Weddell Seal"));
-//разделяем массив на массивы первых и вторых слов
-foreach ($allAnimals as $continents => $animals) {
-    foreach ($animals as $key => $val) {
-        if (strpos ( $val, ' ')) {
-            $firstWords["$continents"][$key] =  substr($val, 0 , strpos ( $val, ' '));
-            $secondWords["$continents"][$key] = trim(substr($val, (strpos ($val, ' '))));
-        }
+$animals = [
+    "Africa"     => ["Philantomba monticola", "Lemuridae", "Panthera pardus"],
+    "Australia"  => ["Sarcophilus laniarius", "Canis dingo", "Vombatidae"],
+    "Antarctica" => ["Phocidae", "Balaenoptera musculus", "Oceanodroma leucorhoa"]
+           ];
+foreach ($animals as $k => $v){
+    $animalsTwoWords[$k] = preg_grep("~\s{1}~",$v);
+}
+foreach ($animalsTwoWords as $key => $value) {
+    foreach($value as $word) {
+        $kusok = explode(" ",$word);
+        $kusok1[] = $kusok[0]. "_" .$key;
+        $kusok2[]= $kusok[1];
     }
 }
-//создаем одномерный массив вторых слов
-$onlySecondsWords = array();
-foreach($secondWords as $arr1) {
-    $onlySecondsWords = array_merge($onlySecondsWords, array_values($arr1));
-}
-//перемешиваем массив первых слов
-foreach ($firstWords as $key => $val) {
-    shuffle($firstWords[$key]);
-}
-//перемешиваем массив вторых слов
-shuffle($onlySecondsWords);
-//выводим на экран
-$q=0;
-foreach ($firstWords as $key => $val) {
-    echo "<h2>$key</h2>";
-    for($i = 0; $i<count($val); $i++) {
-        echo $val[$i]." ";
-        echo $onlySecondsWords[$q];
-        if ($i<count($val)-1) echo ", ";
-        $q++;
-    }
+shuffle($kusok2);
+shuffle($kusok1);
+$count = count($kusok1);
+
+for ($i=0; $i<$count; $i++) {
+    $result = explode("_",$kusok1[$i]);
+    echo "<h2>" . $result[1] ."</h2>" . "<p>" . $result[0] . " ". $kusok2[$i] . "</p>";
 }
 
