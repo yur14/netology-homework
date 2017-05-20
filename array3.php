9a267f60b07978717242dc8ee87514846132c152
yur14 <yurii.simchuk@yahoo.com>
2017-05-20 19:23:34 +0300

    Update array3.php

--- array3.php
+++ array3.php
@@ -1,25 +1,64 @@
 <?php
-$animals = [
-    "Africa"     => ["Philantomba monticola", "Lemuridae", "Panthera pardus"],
-    "Australia"  => ["Sarcophilus laniarius", "Canis dingo", "Vombatidae"],
-    "Antarctica" => ["Phocidae", "Balaenoptera musculus", "Oceanodroma leucorhoa"]
-           ];
-foreach ($animals as $k => $v){
-    $animalsTwoWords[$k] = preg_grep("~\s{1}~",$v);
-}
-foreach ($animalsTwoWords as $key => $value) {
-    foreach($value as $word) {
-        $kusok = explode(" ",$word);
-        $kusok1[] = $kusok[0]. "_" .$key;
-        $kusok2[]= $kusok[1];
+$worldAnimals = [
+        'Europe' => ['Panthera tigris altaica', 'Pusa sibirica', 'Meles meles', 'Ursus maritimus', 'Ursus meles maritimus sibirica'],
+        'North America' => ['Ovis nivicola', 'Lynx rufus', 'Rangifer tarandus', 'Canis latrans', 'Grizzly bear', 'Grizzly rufus nivicola'],
+        'Africa' => ['Rhinocerotidae', 'Loxodonta africana', 'Pardus', 'Lemuridae', 'Lemuridae pardus africana'],
+        'South America' => ['Polar bears', 'Gray volf', 'Black bear', 'Cougar', 'Moose', 'Gray moose bear']
+               ];
+$animalFirst = [];
+$animalSecond = [];
+$animal = [];
+foreach ($worldAnimals as $keys => $values){
+    foreach($values as $key => $val){
+        // Разбиваем входные данные на слова, если 2 то true
+        $quant = count(explode(" ", $val)) == 2;
+        // Если входные данные равняются 2 то разбиваем данные по словам и помещаем в массив слов
+        if($quant){
+            $it = explode(" ", $val);
+            foreach($it as $k => $v){
+                $animal[] = $v;
+                // Если ключ = 0, то это первое слово, помещаем в массив с первыми словами, иначе в массив вторых слов.
+                if($k == 0){
+                    $animalFirst[] = $v;
+                    } else {
+                    $animalSecond[] = $v;
+                }
+            }
+        }
     }
 }
-shuffle($kusok2);
-shuffle($kusok1);
-$count = count($kusok1);
-
-for ($i=0; $i<$count; $i++) {
-    $result = explode("_",$kusok1[$i]);
-    echo "<h2>" . $result[1] ."</h2>" . "<p>" . $result[0] . " ". $kusok2[$i] . "</p>";
+// Перемешиваем массивы данных первых и вторых слов.
+shuffle($animalFirst);
+shuffle($animalSecond);
+$countAnimalFirst = count($animalFirst);
+$length = count($animal) - 1;
+function rnd($arr){
+    return rand(0,$arr - 1);
 }
-
+$parentCount=[];
+// Создаем массив с ключами названиями регионов
+foreach($worldAnimals as $key => $val){
+    $parentCount[$key] = "";
+}
+for($i = 0; $i < count($animalFirst); $i++){
+    foreach($worldAnimals as $key => $val){
+        foreach($val as $k => $v){
+            $quant = count(explode(" ", $v)) == 2; // Проверяем, что бы название состояло из 2х слов
+            $pos = strpos($v, $animalFirst[$i]) === 0; // Проверяем что бы слово было первым
+            if($quant && $pos){
+                $parentCount[$key][] = $animalFirst[$i] . " " . $animalSecond[$i];
+            }
+        }
+    }
+}
+// Выводим полученный массив выдуманных животных с изначальным континентом.
+foreach($parentCount as $key => $val){
+    echo "<h2> $key:</h2> <ol>";
+    foreach($val as $k => $v){
+        if($i < count($v)-1){
+            echo "<li> $v,</li>";
+        }
+    }
+        echo "</ol>";
+        echo implode(", ", $val), ".";
+}
