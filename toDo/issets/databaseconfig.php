bccb5ba09b89ee0c9b1cb442be30b014ed6c23ea
yur14 <yurii.simchuk@yahoo.com>
2017-05-22 15:13:18 +0300

    Create databaseconfig.php

---
+++ toDo/issets/databaseconfig.php
@@ -0,0 +1,9 @@
+<?php
+$configDbGlobalTasks = array(
+    'db_type' => 'mysql',
+    'db_host' => 'localhost',
+    'db_name' => 'simchuk',
+    'db_username' => 'simchuk',
+    'db_password' => 'neto1038'
+);
+$objTasks = new WorkWithDatabase($configDbGlobalTasks);
