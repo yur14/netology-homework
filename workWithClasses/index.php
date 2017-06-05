bda08d69065e59b8f5309a99af6684af3031f0b6
yur14 <yurii.simchuk@yahoo.com>
2017-06-05 14:23:12 +0300

    Update index.php

--- workWithClasses/index.php
+++ workWithClasses/index.php
@@ -1,37 +1,64 @@
 <?php
-require_once __DIR__ . '/ClassNews.php';
-$news = new NewsClass();
-
-$referer = !empty($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-if (stristr($referer, 'ClassNews.php') && !empty($_SESSION['news'])) {
-    $allNews = $functions->pushNewsInFile($_SESSION['news']);
-    header('refresh: 0');
-}
+include 'class/News.php';
+include 'class/Comments.php';
+$news[0] = new News(
+    '0',
+    '26.11.2016',
+    'Новость 1',
+    '<a href="#">news.com</a>',
+    '<p>СМИ опубликовали новость</p>
+	<p>Публикации появились</p>
+<p>Все публикации основывались </p>
+<p>Большинство изданий отредактировали свои сообщения.</p>
+');
+$news[1] = new News(
+    '1',
+    '26.11.2016',
+    'Новость 2',
+    '<a href="#">news.com</a>',
+    '<p>СМИ опубликовали новость</p>
+	<p>Публикации появились</p>
+<p>Все публикации основывались </p>
+<p>Большинство изданий отредактировали свои сообщения.</p>
+');
+$news[2] = new News(
+    '2',
+    '26.11.2016',
+    'Новость 3',
+    '<a href="#">news.com</a>',
+    '<p>СМИ опубликовали новость</p>
+	<p>Публикации появились</p>
+<p>Все публикации основывались </p>
+<p>Большинство изданий отредактировали свои сообщения.</p>
+');
+$comments[0][0] = new Comment('26.11.2016', 'Вася', '<p>Круто!</p>');
+$comments[1][0] = new Comment('26.11.2016', 'WhiteRabbit001', '<p>Хахаха!</p>');
+$comments[1][1] = new Comment('26.11.2016', 'Jason', '<p>Ну вообще...</p>');
 ?>
+
 <!doctype html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Lesson 3.1. Classes and objects</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>News</title>
+    <style>
+        table {
+            width: 50%;
+        }
+        th, td {
+            padding: 10px;
+            border: 1px solid #ddd;
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
-<h1>Главная</h1>
-<p>
-    Инкапсуляция - это свойство системы в ООП, группировать свойства и методы
-    над одним объектом в одном не доступном для обычного пользователя месте(файле).
-    Доступ к данным должен осуществляться через специальные методы доступа (access methods)
-    – «геттеры» (методы получения данных, Get) и «сеттеры» (методы установки данных, Set).
-    К плюсам объектов можно отнести удобства работы с кодом, рефакторинг,
-    чтение при больших объемах кода, повышение безопасноти. К минусам отнесу только
-    то что ООП надо применять по назначению.
-</p>
-
-<a href="index.php?id=0">Первая новость</a>
-<a href="index.php?id=1">Вторая новость</a>
-<a href="index.php?id=2">Третья новость</a>
-
-<h2><?php echo $news->getTitle(); ?></h2>
-<h3><?php echo $news->getText(); ?></h3>
-<h4><?php echo $news->getComment(); ?></h4>
+<a href="index.html"><h3>На главную</h3></a>
+<?php $news[0]->showNews($comments) ?>
+<?php $news[1]->showNews($comments) ?>
+<?php $news[2]->showNews($comments); ?>
 </body>
 </html>
